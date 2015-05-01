8fac62d8dc5c5ff13d15fb4dcd850ee3984eb5d2
Gilbert Fulmore <gilbertfulmore@gmail.com>
2015-04-30 20:56:24 -0700

    Changed AI Behaviour
    Changes AI to only move once ball passes center line

--- src/pong/objects/PaddleAI.java
+++ src/pong/objects/PaddleAI.java
@@ -1,11 +1,8 @@
 package pong.objects;
 
-import java.util.Random;
-
 public class PaddleAI {
 	Paddle aiPaddle;
 	Ball ball;
-	Random rn = new Random();
 	
 	public PaddleAI(Paddle aiPaddle, Ball ball) {
 		this.aiPaddle = aiPaddle;
@@ -13,31 +10,18 @@ public class PaddleAI {
 	}
 	
 	public void movePaddle() {
-		
-		int rand = rn.nextInt((10 - 0) + 10) + 0;
-		if(ball.getE()) {
-			if(!(aiPaddle.getY1() <=0)) {
-				if(ball.getY2() < aiPaddle.getY1()+35) {
-					if(rand == 0) {
-					} else if(rand == 1 || rand == 2) {
-						aiPaddle.setY1(aiPaddle.getY1()-2);
-					} else {
-						aiPaddle.setY1(aiPaddle.getY1()-1);
-					}
+		if(ball.getE() && ball.getX1() > 300) {
+			if(!(aiPaddle.getY1() <= 0)) {
+				if(ball.getY2() < aiPaddle.getY1() + 35) {
+					aiPaddle.setY1(aiPaddle.getY1() - 1);
 				}
 			}
 			if(!(aiPaddle.getY2() > 350)) {
-				if(ball.getY1() > aiPaddle.getY1()+35) {
-					if(rand == 0) {
-					} else if(rand == 1 || rand == 2) {
-						aiPaddle.setY1(aiPaddle.getY1()+2);
-					} else {
-						aiPaddle.setY1(aiPaddle.getY1()+1);
-					}
+				if(ball.getY1() > aiPaddle.getY1() + 35) {
+					aiPaddle.setY1(aiPaddle.getY1() + 1);
 				}
 			}
 		}
-		
 	}	//end movePaddle()
 	
 }	//end class
