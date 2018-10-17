c05962c691a0e52859cbdaac2618f5175106bdeb
Gilbert Fulmore <gilbertfulmore@gmail.com>
2018-10-17 11:52:25 -0700

    Update Ball.java
    Formatting

--- src/pong/objects/Ball.java
+++ src/pong/objects/Ball.java
@@ -175,26 +175,68 @@ public class Ball {
         g.fillOval(x1, y1, 10, 10);
     }
     
-    public int getX1()          { return x1; }
-    public int getY1()          { return y1; }
-    public int getX2()          { return x2; }
-    public int getY2()          { return y2; }
-    public int getBallSlope()   { return ballSlope; }
-    public boolean getN()       { return n; }
-    public boolean getS()       { return s; }
-    public boolean getE()       { return e; }
-    public boolean getW()       { return w; }
-    public int getPlayerScore() { return playerScore; }
-    public int getAiScore()     { return aiScore; }
+    public int getX1() { 
+        return x1; 
+    }
+    public int getY1() { 
+        return y1; 
+    }
+    public int getX2() { 
+        return x2; 
+    }
+    public int getY2() { 
+        return y2; 
+    }
+    public int getBallSlope() { 
+        return ballSlope; 
+    }
+    public boolean getN() { 
+        return n; 
+    }
+    public boolean getS() { 
+        return s; 
+    }
+    public boolean getE() { 
+        return e; 
+    }
+    public boolean getW() { 
+        return w; 
+    }
+    public int getPlayerScore() { 
+        return playerScore; 
+    }
+    public int getAiScore() { 
+        return aiScore; 
+    }
     
-    public void setX1(int x1)                   { this.x1 = x1; }
-    public void setY1(int y1)                   { this.y1 = y1; }
-    public void setX2(int x2)                   { this.x2 = x2; }
-    public void setY2(int y2)                   { this.y2 = y2; }
-    public void setBallSlope(int ballSlope)     { this.ballSlope = ballSlope; }
-    public void getS(boolean s) 		{ this.n = s; }
-    public void getE(boolean e) 		{ this.n = e; }
-    public void getW(boolean w) 		{ this.n = w; }
-    public void setPlayerScore(int playerScore) { this.playerScore = playerScore; }
-    public void setAiScore(int aiScore) 	{ this.aiScore = aiScore; }
+    public void setX1(int x1) { 
+        this.x1 = x1; 
+    }
+    public void setY1(int y1) { 
+        this.y1 = y1; 
+    }
+    public void setX2(int x2) { 
+        this.x2 = x2; 
+    }
+    public void setY2(int y2) { 
+        this.y2 = y2; 
+    }
+    public void setBallSlope(int ballSlope) { 
+        this.ballSlope = ballSlope; 
+    }
+    public void getS(boolean s) { 
+        this.n = s; 
+    }
+    public void getE(boolean e) { 
+        this.n = e; 
+    }
+    public void getW(boolean w) { 
+        this.n = w; 
+    }
+    public void setPlayerScore(int playerScore) { 
+        this.playerScore = playerScore; 
+    }
+    public void setAiScore(int aiScore) { 
+        this.aiScore = aiScore; 
+    }
 }
